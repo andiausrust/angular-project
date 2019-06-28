0ef7cdb49773b2a7e1b3cf803fead0b3d9e88385
Andi <familie.mayer@aon.at>
2019-06-28 16:36:29 +0200

    new project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppinglistComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
